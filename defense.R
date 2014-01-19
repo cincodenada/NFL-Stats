b6fff8be3166ef5d928e0152196541dc726aded4
Joel Bradshaw <cincodenada@gmail.com>
2014-01-18 23:34:20 -0800

    Add postseason data.  Also fix a couple bugs in the R file

--- defense.R
+++ defense.R
@@ -1,17 +1,21 @@
 library('png')
 library('ggplot2')
-read.table('stats.tsv',sep="\t",header=TRUE)
+nflstats = read.table('combined.tsv',sep="\t",header=TRUE)
 plot(nflstats$Pts.G,nflstats$Yds.G,ann=FALSE)
 title(main="NFL Defenses (Regular Season)",
 xlab="Points Allowed Per Game",
 ylab="Yards Allowed Per Game"
 )
 
-xyscale = par("cxy")
-scale = 30
+plotsize = par("usr")
+plotunits = c((plotsize[2]-plotsize[1]),(plotsize[4]-plotsize[3]))
+aspect = par("pin")/plotunits
+aspectratio = aspect[1]/aspect[2]
+xyscale = c(1/aspectratio,1)
+shrink = 150
 for(ii in 1:32) {
-  logo =readPNG(paste(sep="",'logos/',nflstats$Team[ii],'.png'))
-  dims = dim(logo)[1:2]/xyscale/scale
+  logo = readPNG(paste(sep="",'logos/',nflstats$Team[ii],'.png'))
+  dims = dim(logo)[1:2]/xyscale/shrink
   rasterImage(
     logo,
     nflstats$Pts.G[ii]-dims[2]/2,
